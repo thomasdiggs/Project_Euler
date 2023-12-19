602c9f878fdd60c20d9626bc7d2b7484a4c0c422
Thomas Diggs <thomasdiggs@gmail.com>
2023-12-19 15:33:59 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectEuler
+{
+    internal class Program
+    {
+        public static void Problem6()
+        {
+            int sumOfSquares = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sumOfSquares += (i * i);
+            }
+            int squareOfSum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                squareOfSum += i;
+            }
+            squareOfSum *= squareOfSum;
+            int difference = squareOfSum - sumOfSquares;
+            Console.WriteLine("Problem 6: " + difference);
+        }
+
+        static void Main(string[] args)
+        {
+            Problem6();
+        }
+
+    }
+}
