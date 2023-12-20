a8c41caf41751788b91853a645a3a2dd47da0c33
Thomas Diggs <thomasdiggs@gmail.com>
2023-12-19 19:34:28 -0700

    Added solutions to problems 1, 2, and 3

--- Program.cs
+++ Program.cs
@@ -2,6 +2,52 @@
 {
     internal class Program
     {
+        public static void Problem1()
+        {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if ((i%3 == 0) || (i%5 == 0))
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("Problem 1: " + sum);
+        }
+        public static void Problem2()
+        {
+            int sum = 0;
+            int a = 1;
+            int b = 2;
+            while (b < 4000000)
+            {
+                if (b % 2 == 0)
+                {
+                    sum += b;
+                }
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            Console.WriteLine("Problem 2: " + sum);
+        }
+        public static void Problem3()
+        {
+            long n = 600851475143;
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    n /= i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine("Problem 3: " + n);
+        }
         public static void Problem6()
         {
             int sumOfSquares = 0;
@@ -21,6 +67,9 @@
 
         static void Main(string[] args)
         {
+            Problem1();
+            Problem2();
+            Problem3();
             Problem6();
         }
 
