40c38caddd8fbd30a9eae0fdce773d249c0b447d
Thomas Diggs <thomasdiggs@gmail.com>
2023-12-20 16:13:31 -0700

    Added solution to problem 6

--- Program.cs
+++ Program.cs
@@ -112,6 +112,27 @@
             int difference = squareOfSum - sumOfSquares;
             Console.WriteLine("Problem 6: " + difference);
         }
+        public static void Problem7()
+        {
+            ICollection<int> primes = new List<int>();
+            int i = 2;
+            while (primes.Count < 10_001)
+            {
+                for (int j = 2; j <= i; j++)
+                {
+                    if (i % j == 0 && i != j)
+                    {
+                        break;
+                    }
+                    else if (i == j)
+                    {
+                        primes.Add(i);
+                    }
+                }
+               i++;
+            }
+            Console.WriteLine("Problem 7: " + primes.Last());
+        }
 
         static void Main(string[] args)
         {
@@ -121,7 +142,7 @@
             Problem4();
             Problem5();
             Problem6();
+            Problem7();
         }
-
     }
 }
